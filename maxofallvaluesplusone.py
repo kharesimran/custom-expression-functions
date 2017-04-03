9a111c8ddc010a4545c694ba87679c65352b3c2c
Simran Khare <SIMRAN001@e.ntu.edu.sg>
2017-04-03 14:17:09 +0200

    Add files via upload

---
+++ maxofallvaluesplusone.py
@@ -0,0 +1,13 @@
+"""
+Define new functions using @qgsfunction. feature and parent must always be the
+last args. Use args=-1 to pass a list of values as arguments
+"""
+
+from qgis.core import *
+from qgis.gui import *
+from qgis.utils import *
+
+@qgsfunction(args=0, group='Custom')
+def maxofallvaluesplusone(value1, feature, parent):
+	layer=iface.activeLayer()
+	return layer.maximumValue(layer.fieldNameIndex('fid')) + 1
