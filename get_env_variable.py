f863bdf4c30afade81050fc090601c9e06f89723
Simran Khare <SIMRAN001@e.ntu.edu.sg>
2017-04-13 10:41:54 +0200

    Add files via upload

---
+++ get_env_variable.py
@@ -0,0 +1,12 @@
+from qgis.core import *
+from qgis.gui import *
+from qgis.utils import *
+
+@qgsfunction(args='auto', group='Custom')
+def env(var_name, feature, parent):
+    """ Returns the value of the variable 'var_name' 
+        Example usage: env('user_full_name') """
+    active_layer = iface.activeLayer()
+    return QgsExpressionContextUtils.layerScope(active_layer).variable(var_name) \
+         or QgsExpressionContextUtils.projectScope().variable(var_name) \
+         or QgsExpressionContextUtils.globalScope().variable(var_name)
