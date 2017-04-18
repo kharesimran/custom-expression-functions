62169b727516e39ad8942a30c9dab0b860155968
Simran Khare <SIMRAN001@e.ntu.edu.sg>
2017-04-18 15:00:18 +0200

    Add files via upload

---
+++ label_features.py
@@ -0,0 +1,28 @@
+from qgis.core import *
+from qgis.gui import *
+
+"""
+Example: concat("name", ', ', "iso_a2", ': ', label_pop_growth() )
+"""
+@qgsfunction(args=0, group='Custom_label')
+def label_pop_growth(value1, feature, parent):
+	name = feature['name']
+	country_code = feature['iso_a2']
+	pop_max = feature['pop_max']
+	pop_min = feature['pop_min']
+	pop_growth = float(pop_max)/pop_min
+	return "{0:.2f}".format(pop_growth)
+
+
+"""
+Example: concat("name", ', ', "iso_a2", ', ', label_hemisphere() )
+"""
+@qgsfunction(args=0, group='Custom_label')
+def label_hemisphere(value1, feature, parent):
+	name = feature['name']
+	country_code = feature['iso_a2']
+	if feature['latitude'] > 0:
+		hemisphere = 'N'
+	else:
+		hemisphere = 'S'
+	return hemisphere
